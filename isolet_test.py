677011f23f0f681a82e42b71633db0c5ac83b9da
Daniel Casañas <jdaniel.casanasr@gmail.com>
2024-01-18 17:57:47 -0600

    ToDo: Read ISOLET Dataset

--- isolet_test.py
+++ isolet_test.py
@@ -1,10 +1,14 @@
+#import codecs
+#import pandas as pd
+
 # Define the path to your training and testing data
-train_data_path = '/home/jdcasanasr/Downloads/isolet1+2+3+4.data.Z'
-test_data_path  = '/home/jdcasanasr/Downloads/isolet5.data.Z'
+train_data_path = '/home/jdcasanasr/Downloads/isolet1+2+3+4.data'
+test_data_path  = '/home/jdcasanasr/Downloads/isolet5.data'
 
 def load_data(file_path):
     data = []
     with open(file_path, 'r') as file:
+    #with codecs.open(file_path, 'r', encoding='utf-8', errors='ignore') as file:
         for line in file:
             # Split the line into individual values and convert them to floats
             values = [float(val) for val in line.strip().split(',')]
@@ -15,5 +19,20 @@ def load_data(file_path):
 train_data = load_data(train_data_path)
 test_data = load_data(test_data_path)
 
-print(train_data.head())
-print(test_data.head())
+#print(train_data.head())
+#print(test_data.head())
+#print(train_data)
+print(test_data)
+
+
+
+# Define the path to your training and testing data
+#train_data_path = 'path/to/isolete.train.csv'
+#test_data_path = 'path/to/isolete.test.csv'
+
+# Load the data into DataFrames
+#train_data  = pd.read_csv(train_data_path, header=None)
+#test_data   = pd.read_csv(test_data_path, header=None)
+#
+#print(train_data.head())
+#print(test_data.head())
