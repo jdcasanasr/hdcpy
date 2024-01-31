43e894f56a0d57f349617d0618a92f1145480778
Daniel Casañas <jdaniel.casanasr@gmail.com>
2024-01-30 22:28:46 -0600

    ToDo: Unify code as a function

--- isolet_test.py
+++ isolet_test.py
@@ -1,10 +1,3 @@
-#import codecs
-#import pandas as pd
-
-# Define the path to your training and testing data
-train_data_path = '/home/jdcasanasr/Downloads/isolet1+2+3+4.data'
-test_data_path  = '/home/jdcasanasr/Downloads/isolet5.data'
-
 def load_data(file_path):
     data = []
     with open(file_path, 'r') as file:
@@ -15,14 +8,44 @@ def load_data(file_path):
             data.append(values)
     return data
 
+
+
+# Define the path to your training and testing data
+train_data_path = 'data/isolet1+2+3+4.data'
+test_data_path  = 'data/isolet5.data'
+
 # Load the training and testing data
 train_data = load_data(train_data_path)
 test_data = load_data(test_data_path)
 
+sample_array    = []
+class_array     = []
+
+for data_string in train_data:
+    sample_array.append(data_string[0:616])
+    class_array.append(data_string[617])
+
+print(class_array)
+
+#for _ in train_data:
+#    print(train_data[index])
+#    index += step
+
+#isolet_dataset  = open(train_data_path, 'r')
+#isolet_lines    = isolet_dataset.readlines()
+#isolet_data     = []
+#value_data_array = []
+#
+#for line in isolet_lines:
+#    for value in line.strip().split(','):
+#        value_data_array.append(float(value))
+#
+#    isolet_data.append(value_data_array)
+
 #print(train_data.head())
 #print(test_data.head())
 #print(train_data)
-print(test_data)
+#print(test_data)
 
 
 
