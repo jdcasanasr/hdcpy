f7393b8e301c74357d7788c88cf52753461eb4be
Daniel Casañas <jdaniel.casanasr@gmail.com>
2024-02-23 16:02:12 -0600

    ToDo: Take missing class instances into account

--- training.py
+++ training.py
@@ -1,53 +1,57 @@
 from hdcpy import *
+import pandas as pd
+import numpy as np
 
-alphabet = {
+feature_matrix  = pd.read_csv("data/X_train.txt", delim_whitespace=True, header=None).values
+class_vector    = pd.read_csv("data/y_train.txt", delim_whitespace=True, header=None).values
 
-    1   : 'a',
-    2   : 'b',
-    3   : 'c',
-    4   : 'd',
-    5   : 'e',
-    6   : 'f',
-    7   : 'g',
-    8   : 'h',
-    9   : 'i',
-    10  : 'j',
-    11  : 'k',
-    12  : 'l',
-    13  : 'm',
-    14  : 'n',
-    15  : 'o',
-    16  : 'p',
-    17  : 'q',
-    18  : 'r',
-    19  : 's',
-    20  : 't',
-    21  : 'u',
-    22  : 'v',
-    23  : 'w',
-    24  : 'x',
-    25  : 'y',
-    26  : 'z'
-}
+# Get useful parameters.
+number_of_features  = np.shape(feature_matrix)[1]
+number_of_classes   = np.max(class_vector)
 
-training_data_path              = 'data/isolet1+2+3+4.data'
-feature_matrix, class_vector    = load_dataset(training_data_path)
+# Training parameters.
+number_of_dimensions            = 10000
+number_of_levels                = 10
+level_lower_limit               = -1.0
+level_upper_limit               = 1.0
+number_of_positions             = number_of_features
 
-# Training parameters for the ISOLET dataset.
-number_of_dimensions        = 10000
-number_of_classes           = 26
-number_of_levels            = 10
-number_of_positions         = 616
-number_of_features          = number_of_positions
-level_lower_limit           = -1.0
-level_upper_limit           = 1.0
-number_of_class_instances   = 240
+class_instance_vector           = np.zeros(number_of_classes, np.uint)
 
-level_hypermatrix           = get_level_hypermatrix(number_of_levels, number_of_dimensions)
-position_hypermatrix        = get_position_hypermatrix(number_of_positions, number_of_dimensions)
-quantization_range          = get_quantization_range(level_lower_limit, level_upper_limit, number_of_levels)
+# Compute the number of instances per class.
+for class_index in range(1, number_of_classes + 1):
+    for class_instance in class_vector:
+        if class_instance == class_index:
+            class_instance_vector[class_index - 1] += 1
 
-associative_memory          = np.empty((number_of_classes, number_of_dimensions), np.bool_)
-prototype_tensor            = np.empty((number_of_class_instances, number_of_dimensions, number_of_classes), np.bool_)
+number_of_instances_per_class = np.max(class_instance_vector)
 
-print('Hello!')
+print(class_instance_vector)
+
+#level_hypermatrix               = get_level_hypermatrix(number_of_levels, number_of_dimensions)
+#position_hypermatrix            = get_position_hypermatrix(number_of_positions, number_of_dimensions)
+#quantization_range              = get_quantization_range(level_lower_limit, level_upper_limit, number_of_levels)
+#
+#associative_memory              = np.empty((number_of_classes, number_of_dimensions), np.bool_)
+#prototype_hypermatrix           = np.empty((number_of_instances_per_class, number_of_dimensions), np.bool_)
+#
+## Iterate over each class.
+#for class_key in range(1, number_of_classes + 1): # Range = [1, 26]
+#    prototype_index = 0
+#
+#    # Iterate over 'class_vector'.
+#    for class_vector_index in range(len(class_vector)): # Range = [0, 6237]
+#        # Train the model for each class in order.
+#        if (alphabet[class_key] == alphabet[int(class_vector[class_vector_index])]) and (prototype_index < number_of_instances_per_class):
+#            prototype_hypermatrix[prototype_index] = encode(feature_matrix[class_vector_index], quantization_range, level_hypermatrix, position_hypermatrix)
+#            prototype_index += 1 # Range = [0, 239]
+#            print(f'Training for class {alphabet[class_key]} | Found {prototype_index} of 240 Instances.', end = '\r')
+#
+#    # Build the class hypervector.
+#    associative_memory[class_key - 1] = multibundle(prototype_hypermatrix) # Range = [0, 25]
+#
+## Backup data for inference_test.py.
+#np.save("level_hypermatrix", level_hypermatrix)
+#np.save("position_hypermatrix", position_hypermatrix)
+#np.save("quantization_range", quantization_range)
+#np.save("associative_memory", associative_memory)
