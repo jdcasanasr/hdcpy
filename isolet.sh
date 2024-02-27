22be96d1535d5f639c12ff4df433a62182262157
Daniel Casañas <jdaniel.casanasr@gmail.com>
2024-02-27 16:09:54 -0600

    ToDo: Run another dataset

--- isolet.sh
+++ isolet.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-# Loop through input arguments
-for i in {1..3}
+for i in {2..10}
+do
+    for j in {1000..10000..1000}
     do
-        rm -rf __pycache__
-        python3 isolet_unified_test.py 2000 9
+        echo "Number of Dimensions = $j | Number of Quantization Levels = $i"
+        for k in {1..3}
+        do
+            rm -rf __pycache__
+            python3 isolet_unified_test.py "$j" "$i"
+        done
     done
+done
--- ucihar.sh
+++ ucihar.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 # Check if two arguments are provided
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <argument1> <argument2>"
-    exit 1
-fi
+#if [ "$#" -ne 2 ]; then
+#    echo "Usage: $0 <argument1> <argument2>"
+#    exit 1
+#fi
 
 # Assign command-line arguments to variables
-arg1=$1
-arg2=$2
+#arg1=$1
+#arg2=$2
 
 # Loop through input arguments
-for i in {1..3}
+for i in {3..4}
 do
-    rm -rf __pycache__
-    python3 ucihar_unified_test.py "$arg1" "$arg2"
+    for j in {1000..10000..1000}
+    do
+        echo "D = $j | Q = $i"
+        for k in {1..3}
+        do
+            rm -rf __pycache__
+            python3 ucihar_unified_test.py "$j" "$i"
+        done
+    done
 done
