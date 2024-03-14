9ffb8cc895ec8ed287f2d96fe8bb4202b770d489
Daniel Casañas <jdaniel.casanasr@gmail.com>
2024-03-14 14:56:46 -0600

    ToDo: Correct "index x is out of bounds" bug

--- hdcpy.py
+++ hdcpy.py
@@ -3,9 +3,39 @@ import os
 
 from sklearn.datasets           import fetch_openml
 from sklearn.model_selection    import train_test_split
-from sklearn.preprocessing      import normalize
+from sklearn.preprocessing      import MinMaxScaler
 
 # Free the user of manually providing its own copy of the dataset.
+#def fetch_dataset(dataset_name: str, save_directory:str, test_proportion:float):
+#    if not os.path.exists(save_directory):
+#        os.makedirs(save_directory)
+#
+#    file_path = os.path.join(save_directory, f'{dataset_name}.csv')
+#
+#    if not os.path.exists(file_path):
+#        dataset         = fetch_openml(name = dataset_name, version = 1, parser = 'auto')
+#        data, target    = np.array(dataset.data).astype(np.float_), np.array(dataset.target).astype(np.int_)
+#
+#        training_features, testing_features, training_labels, testing_labels = train_test_split(data, target, test_size = test_proportion)
+#
+#        # Combine data and target into a single array.
+#        dataset_array = np.column_stack((data, target))
+#
+#        np.savetxt(file_path, dataset_array, delimiter = ',', fmt = '%s')
+#        
+#        return training_features, testing_features, training_labels - 1, testing_labels - 1
+#
+#    else:
+#        # Read pre-existing ".csv" file, split into features and labels
+#        # and return both as numpy arrays.
+#        dataset_array   = np.genfromtxt(file_path, delimiter = ',', dtype = np.float_)
+#        data            = dataset_array[:, :-1]
+#        target          = dataset_array[:, -1].astype(np.int_)
+#
+#        training_features, testing_features, training_labels, testing_labels = train_test_split(data, target, test_size = test_proportion)
+#
+#        return training_features, testing_features, training_labels - 1, testing_labels - 1
+
 def fetch_dataset(dataset_name: str, save_directory:str, test_proportion:float):
     if not os.path.exists(save_directory):
         os.makedirs(save_directory)
@@ -13,28 +43,45 @@ def fetch_dataset(dataset_name: str, save_directory:str, test_proportion:float):
     file_path = os.path.join(save_directory, f'{dataset_name}.csv')
 
     if not os.path.exists(file_path):
-        dataset         = fetch_openml(name = dataset_name, version = 1, parser = 'auto')
-        data, target    = np.array(dataset.data).astype(np.float_), np.array(dataset.target).astype(np.int_)
+        dataset = fetch_openml(name=dataset_name, version=1, parser='auto')
+        data, target = np.array(dataset.data).astype(np.float_), np.array(dataset.target).astype(np.int_)
 
-        training_features, testing_features, training_labels, testing_labels = train_test_split(data, target, test_size = test_proportion)
+        training_features, testing_features, training_labels, testing_labels = train_test_split(data, target, test_size=test_proportion)
 
         # Combine data and target into a single array.
         dataset_array = np.column_stack((data, target))
 
-        np.savetxt(file_path, dataset_array, delimiter = ',', fmt = '%s')
-        
+        np.savetxt(file_path, dataset_array, delimiter=',', fmt='%s')
+
+        # Check if features need to be normalized.
+        if not np.all((data >= 0) & (data <= 1)):
+            scaler = MinMaxScaler()
+            scaler.fit(training_features)
+            training_features = scaler.transform(training_features)
+            scaler.fit(testing_features)
+            testing_features = scaler.transform(testing_features)
+
         return training_features, testing_features, training_labels - 1, testing_labels - 1
 
     else:
         # Read pre-existing ".csv" file, split into features and labels
         # and return both as numpy arrays.
-        dataset_array   = np.genfromtxt(file_path, delimiter = ',', dtype = np.float_)
-        data            = dataset_array[:, :-1]
-        target          = dataset_array[:, -1].astype(np.int_)
+        dataset_array = np.genfromtxt(file_path, delimiter=',', dtype=np.float_)
+        data = dataset_array[:, :-1]
+        target = dataset_array[:, -1].astype(np.int_)
 
-        training_features, testing_features, training_labels, testing_labels = train_test_split(data, target, test_size = test_proportion)
+        training_features, testing_features, training_labels, testing_labels = train_test_split(data, target, test_size=test_proportion)
+
+        # Check if features need to be normalized
+        if not np.all((data >= 0) & (data <= 1)):
+            scaler = MinMaxScaler()
+            scaler.fit(training_features)
+            training_features = scaler.transform(training_features)
+            scaler.fit(testing_features)  # Fitting on testing features as well
+            testing_features = scaler.transform(testing_features)
 
         return training_features, testing_features, training_labels - 1, testing_labels - 1
+
 
 # ToDo: Generalize for other VSA's.
 def random_hypervector(number_of_dimensions:np.uint) -> np.array:
@@ -111,28 +158,6 @@ def get_position_hypermatrix(number_of_positions:np.uint, number_of_dimensions:n
 
 def get_quantization_range(lower_limit:np.double, upper_limit:np.double, number_of_levels:np.uint) -> np.array:
     return np.linspace(lower_limit, upper_limit, number_of_levels)
-
-# Cram the whole dataset into a single array, stripping blanks and
-# commas, and convertig values into floating-point numbers.
-def load_data(file_path):
-    data = []
-    with open(file_path, 'r') as file:
-        for line in file:
-            values = [float(val) for val in line.strip().split(',')]
-            data.append(values)
-    return data
-
-# ToDo: Generalize for any dataset in .csv format.
-def load_dataset(dataset_path):
-    data_matrix     = load_data(dataset_path)
-    feature_matrix  = []
-    class_vector    = []
-
-    for data_vector in data_matrix:
-        feature_matrix.append(data_vector[0:616])
-        class_vector.append(data_vector[617])
-
-    return feature_matrix, class_vector
 
 # Return the level hypervector matching a given sample.
 def get_level_hypervector(feature:np.double, quantization_range:np.array, level_hypermatrix:np.array) -> np.array:
