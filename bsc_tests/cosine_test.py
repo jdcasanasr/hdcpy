aa87f7895adcdd4ef9f81445676a06346c64afce
Daniel Casañas <jdaniel.casanasr@gmail.com>
2024-04-25 12:04:35 -0600

    Deleted non-critical data and folders

--- bsc_tests/cosine_test.py
+++
@@ -1,7 +0,0 @@
-from hdcpy_v2 import *
-
-for _ in range(10):
-    u = random_hypervector(10000, 'MAP')
-    v = random_hypervector(10000, 'MAP')
-
-    print(f'{cosine_similarity(u, v)}')
--- hamming_test.py
+++
@@ -1,7 +0,0 @@
-from hdcpy_v2 import *
-
-for _ in range(10):
-    u = random_hypervector(10000, 'BSC')
-    v = random_hypervector(10000, 'BSC')
-
-    print(f'{hamming_distance(u, v)}')
