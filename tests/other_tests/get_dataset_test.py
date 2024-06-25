4bb5aef0ec114f3c3b232e0415a872c289d89f8d
Daniel Casañas <jdaniel.casanasr@gmail.com>
2024-06-25 11:11:24 -0600

    Removed unnecessary files

--- tests/other_tests/get_dataset_test.py
+++
@@ -1,9 +0,0 @@
-from hdcpy_auxiliary import *
-
-dataset         = 'har'
-save_directory  = '/home/jdcasanasr/Development/hdcpy/data'
-test_proportion = 0.2
-
-X_train, X_test, y_train, y_test = get_dataset(dataset, save_directory, test_proportion)
-
-pass
